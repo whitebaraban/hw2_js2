6effae22ab1ac31fd7fa805ccd6125ce8ecde275
Pavel <barabanofwork@yandex.ru>
2019-04-30 16:35:55 +0300

    Добавил получение товаров по api Сделал формирование списка товаров и рендер html через классы

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 "use strict"
 
-const GOODS = [
-	{title: 'Book', price: 230},
-	{title: 'T-short', price: 50},
-	{title: 'Milk', price: 0.99},
-	{title: 'Potato', price: 0.20},
-	{title: 'Duck', price: 1.99},
-	{title: 'Iphone', price: 200},
-	{title: 'Pen', price: 0.30},
-	{} // Для проверки значений по умолчанию
-];
-
 // function renderGoods(title, price) {
 // 	return '<div class="item"><h2>' + title + '</h2><br>' + '<b>' + price + '<b>' + '</div>'
 // }
@@ -21,47 +10,98 @@ const GOODS = [
 // 		document.querySelector('.goods').innerHTML += renderGoods(rec.title, rec.price); }); 
 // }	
 
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
 
-// Пока что тяжело заранее продумать методы и конструктор класса
-class cart {
-	renderCart () {}
+function makeGETRequest(url, callback) {
+  var xhr;
 
-	addToCart() {}
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { 
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
 
-	removeItemCart () {}
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+   		console.log(xhr.status);
+      	callback(xhr.responseText);
+    }
+  }
 
-	summCart() {}
-
-	clearAllCart() {}
-
+xhr.open('GET', url, true);
+xhr.send();
 }
 
-class itemCart {
-	constructor(tov_name, tov_count, tov_price) {}
-}
 
-
-const renderGoodsItem = (title  = 'Not found', price = "0") => `<div class="item"><h2>${title}</h2>${price}$
-	<button class="addToCartBtn">Добавить</button></div>`;
-
-const renderGoodList = goods => {
-	let goods_list = goods.map(item => renderGoodsItem(item.title, item.price));
-	// От запятых помог избавиться вывод массива по элементу, а не содержание массива целиком с запятыми
-	for (let i = 0; i < goods_list.length; i++) { 
-		document.querySelector('.goods').innerHTML += goods_list[i];
+class GoodsList {
+	constructor() {
+		this.goods = [];
 	}
 
-	// Подсчёт суммы товаров
-	const summ_good = items => {
-		let summ = 0;
-		for (let i = 0; i < items.length; i++) { 
-			if (!isNaN(items[i].price)) {
-				summ += items[i].price;
-			}
-		}
-		console.log(`Сумма всех товаров: ${summ}`)
+ 	fetchGoods() {
+    	makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+      		this.goods = JSON.parse(goods);
+      		console.log(this.goods);
+      		list.render();
+    	})
 	}
-	summ_good(goods)
+
+	render() {
+		let listHtml = '';
+		this.goods.forEach(good => {
+			const goodItem = new GoodsItem(good.product_name, good.price);
+			listHtml += goodItem.render();
+		});
+		document.querySelector('.goods').innerHTML = listHtml;
+	}
 }
 
-renderGoodList(GOODS);
+class GoodsItem {
+	constructor(title, price) {
+		this.title = title;
+		this.price = price;
+	}
+	render() {
+		return `<div class="item"><h2>${this.title}</h2>${this.price}$<button class="addToCartBtn">Добавить</button></div>`
+	}
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+
+
+
+// const GOODS = [
+// 	{title: 'Book', price: 230},
+// 	{title: 'T-short', price: 50},
+// 	{title: 'Milk', price: 0.99},
+// 	{title: 'Potato', price: 0.20},
+// 	{title: 'Duck', price: 1.99},
+// 	{title: 'Iphone', price: 200},
+// 	{title: 'Pen', price: 0.30},
+// 	{} // Для проверки значений по умолчанию
+// ];
+
+// const renderGoodsItem = (title  = 'Not found', price = "0") => `<div class="item"><h2>${title}</h2>${price}$
+// 	<button class="addToCartBtn">Добавить</button></div>`;
+
+// const renderGoodList = goods => {
+// 	let goods_list = goods.map(item => document.querySelector('.goods').innerHTML += 
+// 		renderGoodsItem(item.title, item.price));
+
+// 	const summ_good = items => {
+// 		let summ = 0;
+// 		for (let i = 0; i < items.length; i++) { 
+// 			if (!isNaN(items[i].price)) {
+// 				summ += items[i].price;
+// 			}
+// 		}
+// 		console.log(`Сумма всех товаров: ${summ}`)
+// 	}
+// 	summ_good(goods)
+// }
+
+// renderGoodList(GOODS);
+
+
+
